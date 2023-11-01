d0856a27b77bdeb1d24131c1cf6a96272d4f774d
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-01 18:58:44 +0800

    chore: setup prettier and eslint config + commands

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: [
+    "universe/native",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+  ],
+}
