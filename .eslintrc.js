9b5f0fafd6bcb566a5479e15087face7e4b153a0
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-02 12:14:17 +0800

    fix: remove some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   extends: [
     "universe/native",
-    "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
