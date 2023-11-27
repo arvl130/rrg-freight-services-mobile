14fcc50568cebdeeba60b50d69efea5fc1ac339f
Angelo Geulin <angelogeulin123@gmail.com>
2023-11-28 00:33:27 +0800

    chore: remove unnecessary fragment

--- app/(app)/driver/transfer-shipments/index.tsx
+++ app/(app)/driver/transfer-shipments/index.tsx
@@ -22,64 +22,62 @@ export default function TransferShipmentsPage() {
       {status === "error" && <Text>Error {error.message}</Text>}
       {status === "success" && (
         <View>
-          <>
-            {data.transferShipments.length === 0 ? (
-              <Text
-                style={{
-                  textAlign: "center",
-                }}
-              >
-                No transfer shipments assigned.
-              </Text>
-            ) : (
-              <>
-                {data.transferShipments.map((transferShipment) => (
-                  <View key={transferShipment.id}>
-                    <TouchableOpacity
-                      onPress={() =>
-                        router.push({
-                          pathname: "/(app)/driver/transfer-shipments/[id]/",
-                          params: {
-                            id: transferShipment.id,
-                          },
-                        })
-                      }
-                      activeOpacity={0.6}
-                      style={{
-                        backgroundColor: "black",
-                        borderRadius: 8,
-                        paddingVertical: 12,
-                        paddingHorizontal: 16,
-                      }}
-                    >
-                      <View>
-                        <Text
-                          style={{
-                            color: "white",
-                            fontSize: 24,
-                          }}
-                        >
-                          {transferShipment.id}
-                        </Text>
-                      </View>
-                      <View>
-                        <Text
-                          style={{
-                            color: "white",
-                          }}
-                        >
-                          Assigned:{" "}
-                          {DateTime.fromISO(
-                            transferShipment.createdAt,
-                          ).toLocaleString(DateTime.DATETIME_SHORT)}
-                        </Text>
-                      </View>
-                    </TouchableOpacity>
-                  </View>
-                ))}
-              </>
-            )}
-          </>
+          {data.transferShipments.length === 0 ? (
+            <Text
+              style={{
+                textAlign: "center",
+              }}
+            >
+              No transfer shipments assigned.
+            </Text>
+          ) : (
+            <>
+              {data.transferShipments.map((transferShipment) => (
+                <View key={transferShipment.id}>
+                  <TouchableOpacity
+                    onPress={() =>
+                      router.push({
+                        pathname: "/(app)/driver/transfer-shipments/[id]/",
+                        params: {
+                          id: transferShipment.id,
+                        },
+                      })
+                    }
+                    activeOpacity={0.6}
+                    style={{
+                      backgroundColor: "black",
+                      borderRadius: 8,
+                      paddingVertical: 12,
+                      paddingHorizontal: 16,
+                    }}
+                  >
+                    <View>
+                      <Text
+                        style={{
+                          color: "white",
+                          fontSize: 24,
+                        }}
+                      >
+                        {transferShipment.id}
+                      </Text>
+                    </View>
+                    <View>
+                      <Text
+                        style={{
+                          color: "white",
+                        }}
+                      >
+                        Assigned:{" "}
+                        {DateTime.fromISO(
+                          transferShipment.createdAt,
+                        ).toLocaleString(DateTime.DATETIME_SHORT)}
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </>
+          )}
         </View>
       )}
     </View>
