3c5f3f067dd4466689524a9db11be316d2e4cff4
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-09 12:39:53 +0800

    refactor: move registered fonts to separate file

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,3 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome"
 import {
   DarkTheme,
   DefaultTheme,
@@ -16,6 +15,7 @@ import { Platform, useColorScheme } from "react-native"
 import { AuthProvider } from "@/components/auth"
 import { Notifications } from "@/components/notifications"
 import { useAppState, useOnlineManager } from "@/utils/tanstack-query"
+import { REGISTERED_FONTS } from "@/utils/fonts"
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -31,40 +31,7 @@ export const unstable_settings = {
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require("@/assets/fonts/SpaceMono-Regular.ttf"),
-    "Montserrat-ExtraLight": require("@/assets/fonts/Montserrat/Montserrat-ExtraLight.ttf"),
-    "Montserrat-ExtraLightItalic": require("@/assets/fonts/Montserrat/Montserrat-ExtraLightItalic.ttf"),
-    "Montserrat-Light": require("@/assets/fonts/Montserrat/Montserrat-Light.ttf"),
-    "Montserrat-LightItalic": require("@/assets/fonts/Montserrat/Montserrat-LightItalic.ttf"),
-    "Montserrat-Thin": require("@/assets/fonts/Montserrat/Montserrat-Thin.ttf"),
-    "Montserrat-ThinItalic": require("@/assets/fonts/Montserrat/Montserrat-ThinItalic.ttf"),
-    Montserrat: require("@/assets/fonts/Montserrat/Montserrat-Regular.ttf"),
-    "Montserrat-Italic": require("@/assets/fonts/Montserrat/Montserrat-Italic.ttf"),
-    "Montserrat-Medium": require("@/assets/fonts/Montserrat/Montserrat-Medium.ttf"),
-    "Montserrat-MediumItalic": require("@/assets/fonts/Montserrat/Montserrat-MediumItalic.ttf"),
-    "Montserrat-SemiBold": require("@/assets/fonts/Montserrat/Montserrat-SemiBold.ttf"),
-    "Montserrat-SemiBoldItalic": require("@/assets/fonts/Montserrat/Montserrat-SemiBoldItalic.ttf"),
-    "Montserrat-Bold": require("@/assets/fonts/Montserrat/Montserrat-Bold.ttf"),
-    "Montserrat-BoldItalic": require("@/assets/fonts/Montserrat/Montserrat-BoldItalic.ttf"),
-    "Montserrat-ExtraBold": require("@/assets/fonts/Montserrat/Montserrat-ExtraBold.ttf"),
-    "Montserrat-ExtraBoldItalic": require("@/assets/fonts/Montserrat/Montserrat-ExtraBoldItalic.ttf"),
-    "Montserrat-Black": require("@/assets/fonts/Montserrat/Montserrat-Black.ttf"),
-    "Montserrat-BlackItalic": require("@/assets/fonts/Montserrat/Montserrat-BlackItalic.ttf"),
-    "Roboto-Thin": require("@/assets/fonts/Roboto/Roboto-Thin.ttf"),
-    "Roboto-ThinItalic": require("@/assets/fonts/Roboto/Roboto-ThinItalic.ttf"),
-    "Roboto-Light": require("@/assets/fonts/Roboto/Roboto-Light.ttf"),
-    "Roboto-LightItalic": require("@/assets/fonts/Roboto/Roboto-LightItalic.ttf"),
-    Roboto: require("@/assets/fonts/Roboto/Roboto-Regular.ttf"),
-    "Roboto-Italic": require("@/assets/fonts/Roboto/Roboto-Italic.ttf"),
-    "Roboto-Medium": require("@/assets/fonts/Roboto/Roboto-Medium.ttf"),
-    "Roboto-MediumItalic": require("@/assets/fonts/Roboto/Roboto-MediumItalic.ttf"),
-    "Roboto-Bold": require("@/assets/fonts/Roboto/Roboto-Bold.ttf"),
-    "Roboto-BoldItalic": require("@/assets/fonts/Roboto/Roboto-BoldItalic.ttf"),
-    "Roboto-Black": require("@/assets/fonts/Roboto/Roboto-Black.ttf"),
-    "Roboto-BlackItalic": require("@/assets/fonts/Roboto/Roboto-BlackItalic.ttf"),
-    ...FontAwesome.font,
-  })
+  const [loaded, error] = useFonts(REGISTERED_FONTS)
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
