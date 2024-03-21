79c32df5367992a35f0b462baded939939e8b319
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-21 11:54:03 +0800

    fix: formatting

--- src/components/error-view.tsx
+++ src/components/error-view.tsx
@@ -1,78 +1,77 @@
- import { Text, View, Image, TouchableOpacity } from "react-native"
+import { Text, View, Image, TouchableOpacity } from "react-native"
 
 export function ErrorView(props: { message: string }) {
   return (
     <View
-    style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundColor: 'rgba(0, 0, 0, 0.5)',  
-    }}
-  >
-    <View
       style={{
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
-        elevation: 5, 
-        shadowColor: '#000',  
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        alignItems: 'center',
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
       }}
     >
-      <Image
-        source={require("@/assets/images/error-logo.png")}
+      <View
         style={{
-          width: 40,
-          height: 40,
-          marginBottom: 10,
-        }}
-      />
-      <Text
-        style={{
-          textAlign: "center",
-          fontFamily: "Roboto-Bold",
-          fontSize: 24,
-          marginBottom: 5,
+          backgroundColor: "white",
+          padding: 20,
+          borderRadius: 10,
+          elevation: 5,
+          shadowColor: "#000",
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          alignItems: "center",
         }}
       >
-        Error!
-      </Text>
-      <Text
-        style={{
-          textAlign: "center",
-          fontFamily: "Roboto",
-          fontSize: 16,
-          color: "red",
-          marginBottom: 20,
-        }}
-      >
-       {props.message}
-      </Text>
-      <TouchableOpacity
-        onPress={() => { 
-        }}
-        style={{
-          backgroundColor: '#FF6347',
-          paddingVertical: 10,
-          paddingHorizontal: 20,
-          borderRadius: 5,
-        }}
-      >
+        <Image
+          source={require("@/assets/images/error-logo.png")}
+          style={{
+            width: 40,
+            height: 40,
+            marginBottom: 10,
+          }}
+        />
         <Text
           style={{
+            textAlign: "center",
             fontFamily: "Roboto-Bold",
-            fontSize: 18,
-            color: 'white',
+            fontSize: 24,
+            marginBottom: 5,
           }}
         >
-          OK
+          Error!
         </Text>
-      </TouchableOpacity>
+        <Text
+          style={{
+            textAlign: "center",
+            fontFamily: "Roboto",
+            fontSize: 16,
+            color: "red",
+            marginBottom: 20,
+          }}
+        >
+          {props.message}
+        </Text>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            backgroundColor: "#FF6347",
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            borderRadius: 5,
+          }}
+        >
+          <Text
+            style={{
+              fontFamily: "Roboto-Bold",
+              fontSize: 18,
+              color: "white",
+            }}
+          >
+            OK
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  </View>
-);
+  )
 }
