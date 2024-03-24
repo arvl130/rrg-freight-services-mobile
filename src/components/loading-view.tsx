fe6fa202f5ace0140561c37ee0466d06c4247455
Yumeko <arlenejoy.nacion777@gmail.com>
2024-03-25 00:06:47 +0800

    feat: loading-view ui

--- src/components/loading-view.tsx
+++ src/components/loading-view.tsx
@@ -1,15 +1,38 @@
-import { ActivityIndicator, View } from "react-native"
+import React from "react"
+import { Text, View, Image } from "react-native"
 
-export function LoadingView() {
+export default function App() {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignContent: "center",
+        alignItems: "center",
       }}
     >
-      <ActivityIndicator size="large" />
+      <View
+        style={{
+          backgroundColor: "#fff",
+          padding: 20,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
+        }}
+      >
+        <Image
+          source={require("@/assets/images/pickup-truck.gif")}
+          style={{ width: 50, height: 50 }}
+        />
+        <Text style={{ marginTop: 10 }}>Loading, please wait...</Text>
+      </View>
     </View>
   )
 }
