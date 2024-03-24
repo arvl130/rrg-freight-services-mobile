a2b72df1d2300d7741d218aa27b1b765c932165c
Yumeko <arlenejoy.nacion777@gmail.com>
2024-03-25 00:09:38 +0800

    fix: formatting

--- src/components/loading-view.tsx
+++ src/components/loading-view.tsx
@@ -1,7 +1,6 @@
-import React from "react"
 import { Text, View, Image } from "react-native"
 
-export default function App() {
+export function LoadingView() {
   return (
     <View
       style={{
