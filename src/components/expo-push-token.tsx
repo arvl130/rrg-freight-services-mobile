c3fe785393391e6309b7bf800a5ec55c7a79a259
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-27 19:23:47 +0800

    fix: load token from storage on first mount

--- src/components/expo-push-token.tsx
+++ src/components/expo-push-token.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from "react"
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useEffect, useState } from "react"
 import type { ExpoPushToken } from "expo-notifications"
 import {
   AndroidImportance,
@@ -10,11 +10,14 @@ import { Platform } from "react-native"
 import { isDevice } from "expo-device"
 import { useNotificationPermission } from "./notification-permission"
 import Constants from "expo-constants"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const ExpoPushTokenContext = createContext<{
+  isLoading: boolean
   token: ExpoPushToken | null
   requestToken: () => Promise<null | ExpoPushToken>
 }>({
+  isLoading: true,
   token: null,
   requestToken: async () => {
     return null
@@ -23,18 +26,26 @@ const ExpoPushTokenContext = createContext<{
 
 export function ExpoPushTokenProvider(props: { children: ReactNode }) {
   const { permission } = useNotificationPermission()
+  const [isLoading, setIsLoading] = useState(true)
   const [token, setToken] = useState<null | ExpoPushToken>(null)
 
-  async function requestToken() {
-    if (Platform.OS === "android") {
-      setNotificationChannelAsync("default", {
-        name: "default",
-        importance: AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: "#FF231F7C",
-      })
+  useEffect(() => {
+    async function retrieveSavedToken() {
+      const expoPushTokenData = await AsyncStorage.getItem("expo-push-token")
+      if (expoPushTokenData) {
+        setToken({
+          type: "expo",
+          data: expoPushTokenData,
+        })
+      }
+
+      setIsLoading(false)
     }
 
+    retrieveSavedToken()
+  }, [])
+
+  async function requestToken() {
     if (!isDevice) {
       console.log("Running environment should be a physical device.")
       return null
@@ -45,17 +56,33 @@ export function ExpoPushTokenProvider(props: { children: ReactNode }) {
       return null
     }
 
-    const token = await getExpoPushTokenAsync({
-      projectId: Constants.expoConfig?.extra?.eas.projectId,
-    })
+    if (Platform.OS === "android") {
+      setNotificationChannelAsync("default", {
+        name: "default",
+        importance: AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      })
+    }
 
-    setToken(token)
-    return token
+    try {
+      const token = await getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      })
+
+      await AsyncStorage.setItem("expo-push-token", token.data)
+
+      setToken(token)
+      return token
+    } catch {
+      return null
+    }
   }
 
   return (
     <ExpoPushTokenContext.Provider
       value={{
+        isLoading,
         token,
         requestToken,
       }}
