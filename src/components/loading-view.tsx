52a1cfc080856110e8113e2937c2efb920f227f6
Angelo Geulin <angelogeulin123@gmail.com>
2024-03-28 19:54:38 +0800

    style: make icon bigger

--- src/components/loading-view.tsx
+++ src/components/loading-view.tsx
@@ -19,18 +19,25 @@ export function LoadingView() {
           shadowColor: "#000",
           shadowOffset: {
             width: 0,
-            height: 2,
+            height: 6,
           },
-          shadowOpacity: 0.25,
-          shadowRadius: 3.84,
-          elevation: 5,
+          shadowOpacity: 0.2,
+          shadowRadius: 5.62,
+          elevation: 8,
+          paddingHorizontal: 50,
+          paddingVertical: 25,
         }}
       >
         <Image
           source={require("@/assets/images/pickup-truck.gif")}
-          style={{ width: 50, height: 50 }}
+          style={{ width: 75, height: 75 }}
         />
-        <Text style={{ marginTop: 10 }}>Loading, please wait...</Text>
+        <Text
+          style={{ marginTop: 10, fontFamily: "Roboto-Bold", fontSize: 24 }}
+        >
+          Loading
+        </Text>
+        <Text style={{ marginTop: 10 }}>Please wait ...</Text>
       </View>
     </View>
   )
