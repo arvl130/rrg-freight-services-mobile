8228e67cf50c5474f970a663e183847bb42151a2
Wally Gaynor <walliegaynor29@gmail.com>
2024-04-21 23:01:26 +0800

    fix: Identify which package has an active otp timer

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,7 +2,10 @@ import { create } from "zustand"
 
 type CountTimer = {
   timer: number
+  packagesStored: string[]
   isButtonDisabled: boolean
+  addPackageId: (packageId: string) => void
+  removePackageId: (packageId: string) => void
   reset: () => void
   setDisabled: () => void
   setEnabled: () => void
@@ -10,6 +13,17 @@ type CountTimer = {
 
 export const useCountTimer = create<CountTimer>((set) => ({
   timer: 180,
+  packagesStored: [],
+  addPackageId: (packageId: string) => {
+    set((state) => ({ packagesStored: [...state.packagesStored, packageId] }))
+  },
+  removePackageId: (packageId: string) => {
+    set((state) => ({
+      packagesStored: state.packagesStored.filter((_package) => {
+        return _package !== packageId
+      }),
+    }))
+  },
   isButtonDisabled: false,
   reset: () => set({ timer: 180 }),
   setDisabled: () => {
